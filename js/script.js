3e6d884d108bb81444b96b74b9a250a3ca7b8939
Michael Girgis <mgirgis@duoconsulting.com>
2015-07-10 12:42:53 -0500

    adding work blurb

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@
 var navHeight = $('header').outerHeight();
 
 // Add some smooth scrolling to the banner image
-$('#main-nav a').click(function(e){
+$('#main-nav a, .scroll').click(function(e){
   var href = $(this).attr('href'),
     offsetTop = href === '#' ? 0 : $(href).offset().top - navHeight;
 
